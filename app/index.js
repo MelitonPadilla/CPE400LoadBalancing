88e955f4e79038492a48edd4fa489801fbc0b5e9
nicholes.ben@gmail.com <ben@Benjamins-MacBook-Pro.local>
2016-11-22 23:10:02 -0800

    added nodes

--- app/index.js
+++ app/index.js
@@ -1,24 +1,17 @@
 import './main.css';
-import Graph from './graph.js';
 import Simulation from './graph.js';
 
 main();
 
 function main() {
-    var graph = new Graph();
-    document.getElementById("LB1").onclick = () => {
-        graph.runWithoutAggregation();
-    };
-    document.getElementById("LB2").onclick = () => {
-        graph.runWithAggregation();
     var simulation = new Simulation();
-    document.getElementById("RR").onclick = () => {
-    	simulation.runWithoutAggregation();
-    };
-    document.getElementById("LFU").onclick = () => {
-        simulation.runWithAggregation();
-    };
-    document.getElementById("reset").onclick = () => {
-        graph.reset();
-    };
+    // document.getElementById("RR").onclick = () => {
+    //     simulation.runWithoutAggregation();
+    // };
+    // document.getElementById("LFU").onclick = () => {
+    //     simulation.runWithAggregation();
+    // };
+    // document.getElementById("reset").onclick = () => {
+    //     simulation.reset();
+    // };
 }
