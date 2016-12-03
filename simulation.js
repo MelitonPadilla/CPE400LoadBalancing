ff16f2a1ca67887d4e87f5ea8b1c8f051411d588
nicholes.ben@gmail.com <ben@Benjamins-MacBook-Pro.local>
2016-12-03 15:52:37 -0800

    encapsulated functions

--- simulation.js
+++ simulation.js
@@ -1,43 +1,108 @@
-  var clientRequest = document.getElementById("canvas");
-  var cr = clientRequest.getContext("2d");
+function initialize(){
+  
+  var x = document.getElementById("canvas");
+  var cr = x.getContext("2d");
+  cr.beginPath();
   cr.rect(150,180,50,50);
+  cr.strokeStyle = 'black';
   cr.stroke();
+  cr.closePath();
 
-  var lb1 = document.getElementById("canvas");
-  var circle = lb1.getContext("2d");
+  var circle = x.getContext("2d");
   circle.beginPath();
   circle.arc(500,200,40,0,2*Math.PI);
+  circle.strokeStyle = 'black';
   circle.stroke();
+  circle.closePath();
 
-  var c1=document.getElementById("canvas");
-  var client1=c1.getContext("2d");
+  var client1 = x.getContext("2d");
+  client1.beginPath();
   client1.rect(300,400,80,80);
+  client1.strokeStyle = 'black';
   client1.stroke();
+  client1.closePath();
 
-  var c2=document.getElementById("canvas");
-  var client2=c2.getContext("2d");
+  var client2 = x.getContext("2d");
+  client2.beginPath();
   client2.rect(460,400,80,80);
+  client2.strokeStyle = 'black';
   client2.stroke();
+  client2.closePath();
 
-  var c3=document.getElementById("canvas");
-  var client3=c3.getContext("2d");
+  var client3 = x.getContext("2d");
+  client3.beginPath();
   client3.rect(620,400,80,80);
+  client3.strokeStyle = 'black';
   client3.stroke();
+  client3.closePath();
 
-  var c1line = document.getElementById("canvas");
-  var line1 = c1line.getContext("2d");
+  var line1 = x.getContext("2d");
+  line1.beginPath();
   line1.moveTo(465,220);
   line1.lineTo(350,400);
+  line1.strokeStyle = 'black';
   line1.stroke();
-
-  var c2line = document.getElementById("canvas");
-  var line2 = c2line.getContext("2d");
+  line1.closePath();
+  
+  var line2 = x.getContext("2d");
+  line2.beginPath();
   line2.moveTo(500,240);
   line2.lineTo(500,400);
+  line2.strokeStyle = 'black';
   line2.stroke();
+  line2.closePath();
 
-  var c3line = document.getElementById("canvas");
-  var line3 = c3line.getContext("2d");
+  var line3 = x.getContext("2d");
+  line3.beginPath();
   line3.moveTo(535,220);
   line3.lineTo(655,400);
+  line3.strokeStyle = 'black';
   line3.stroke();
+  line3.closePath();
+
+  } 
+
+initialize();
+
+// bind event handler to clear button
+document.getElementById('reset').addEventListener('click', function() {
+        var canvas = document.getElementById('canvas');
+        var context = canvas.getContext('2d');
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        initialize();
+      }, false);
+
+function drawLine1(color){
+  var x = document.getElementById("canvas");
+  var line1 = x.getContext("2d");
+  line1.beginPath();
+  line1.moveTo(465,220);
+  line1.lineTo(350,400);
+  line1.strokeStyle = color;
+  line1.stroke();
+  line1.closePath();
+}
+
+function drawline2(color){
+  var x = document.getElementById("canvas");
+  var line2 = x.getContext("2d");
+  line2.beginPath();
+  line2.moveTo(500,240);
+  line2.lineTo(500,400);
+  line2.strokeStyle = color;
+  line2.stroke();
+  line2.closePath();
+}
+
+function drawline3(color){
+  var x = document.getElementById("canvas");
+  var line3 = x.getContext("2d");
+  line3.beginPath();
+  line3.moveTo(535,220);
+  line3.lineTo(655,400);
+  line3.strokeStyle = color;
+  line3.stroke();
+  line3.closePath();
+}
+
+
